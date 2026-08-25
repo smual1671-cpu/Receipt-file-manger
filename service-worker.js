9911bdb7904e2199a67fd3f09519a0fa132570e5
smual1671-cpu <smual1671@gmail.com>
2026-08-25 06:38:07 +0530

    Correct file uploaded

---
+++ service-worker.js
@@ -0,0 +1,3 @@
+const CACHE='fuel-receipt-v2';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./style.css','./app.js']))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
